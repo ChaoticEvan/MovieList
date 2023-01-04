7ec1fd51a03ef56d83e39f46da6c514132ec50f6
Evan Voordeckers <evan.voordeckers@yahoo.com>
2023-01-04 15:26:34 -0500

    firebase integrated

--- movieratings/src/components/MovieList.js
+++ movieratings/src/components/MovieList.js
@@ -28,13 +28,13 @@ export default function MovieList() {
                 <tbody>
                     <tr>
                         <td><Movie image="image" title="Movie title 1" director="director" /></td>
-                        <td><Movie image="image" title="Movie title 1" director="director" /></td>
-                        <td><Movie image="image" title="Movie title 1" director="director" /></td>
+                        <td><Movie image="image" title="Movie title 2" director="director" /></td>
+                        <td><Movie image="image" title="Movie title 3" director="director" /></td>
                     </tr>
                     <tr>
-                        <td><Movie image="image" title="Movie title 1" director="director" /></td>
-                        <td><Movie image="image" title="Movie title 1" director="director" /></td>
-                        <td><Movie image="image" title="Movie title 1" director="director" /></td>
+                        <td><Movie image="image" title="Movie title 4" director="director" /></td>
+                        <td><Movie image="image" title="Movie title 5" director="director" /></td>
+                        <td><Movie image="image" title="Movie title 6" director="director" /></td>
                     </tr>
                 </tbody>
             </Table>
--- movieratings/src/index.js
+++ movieratings/src/index.js
@@ -1,5 +1,8 @@
+// React imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Reactstrap imports
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     Navbar,
@@ -7,20 +10,43 @@ import {
     NavbarText,
 } from 'reactstrap';
 import './index.css';
+
+// App imports
 import MovieList from './components/MovieList';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar color="dark" dark>
-                    <NavbarBrand href="/" color="dark">Movie Ratings</NavbarBrand>
-                    <NavbarText>Account</NavbarText>
-                </Navbar>
-                <MovieList />
-            </div>
-        );
-    }
+// Firebase imports
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionDate } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCMZ8zqCS-Z8wZVkCF3ShtZJrFhvrzIog4",
+    authDomain: "movielist-41e99.firebaseapp.com",
+    projectId: "movielist-41e99",
+    storageBucket: "movielist-41e99.appspot.com",
+    messagingSenderId: "1063265011886",
+    appId: "1:1063265011886:web:44f873e7c856aeb03b453f",
+    measurementId: "G-JFQBD9CL7R"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export default function Home() {
+    const user = useAuthState(auth);
+
+    return (
+        <div>
+            <Navbar color="dark" dark>
+                <NavbarBrand href="/" color="dark">Movie Ratings</NavbarBrand>
+                <NavbarText>Account</NavbarText>
+            </Navbar>
+            <MovieList />
+        </div>
+    );
 }
 
 // ========================================
