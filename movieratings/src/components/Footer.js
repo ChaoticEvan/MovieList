60062ca90d98d9b2d0eec75481a5d6c473ba3630
Evan Voordeckers <evan.voordeckers@yahoo.com>
2023-01-11 11:50:47 -0500

    Added linkedin. All buttons open in new tab

--- movieratings/src/components/Footer.js
+++ movieratings/src/components/Footer.js
@@ -1,13 +1,14 @@
 import { Button } from "reactstrap";
-import { BsTwitter, BsGithub } from "react-icons/bs"
+import { BsTwitter, BsGithub, BsLinkedin } from "react-icons/bs"
 import { MdEmail } from "react-icons/md"
 
 export default function Footer() {
     return (
         <footer className="footer">
             <center>
-                <Button href="https://twitter.com/EvanVoordeckers" className="footer-button"><BsTwitter /></Button>
-                <Button href="https://github.com/ChaoticEvan" className="footer-button"><BsGithub /></Button>
+                <Button target="_blank" href="https://www.linkedin.com/in/evan-voordeckers-softwaredev/" className="footer-button"><BsLinkedin /></Button>
+                <Button target="_blank" href="https://twitter.com/EvanVoordeckers" className="footer-button"><BsTwitter /></Button>
+                <Button target="_blank" href="https://github.com/ChaoticEvan" className="footer-button"><BsGithub /></Button>
                 <Button href="mailto:evan.voordeckers@gmail.com" className="footer-button"><MdEmail /></Button>
             </center>
         </footer>
